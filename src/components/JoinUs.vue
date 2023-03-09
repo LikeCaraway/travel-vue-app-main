90667aa9092aaf88742b8eefd87ffea1ee80e90a
zhuang-xie <1610661993@qq.com>
2023-03-09 10:09:07 +0800

    first commit

---
+++ src/components/JoinUs.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="container">
+        <div class="JoinUs-box">
+            <h1 >
+                加入我们
+            </h1>
+            <div class="content">
+                <div class="left">
+                <p>　　你可能从诗句中、书本中、影视作品中和朋友口中看过，听说过不少美景，如果不亲身去看看的话，
+                    那些美景将永远只是听说，。人生最好的旅行，就是你在一个陌生的地方，发现一种久违的感动。
+                     独自旅行，不受羁绊，没有约束。有一天，背上包，趁年轻，带上自己，有多远，走多远。
+                </p>
+                <p>　　或带父母去他们想去但从未去过的地方，或与爱人踏遍天涯海角，或带“雏鸟”领略大好山河。
+                    与亲朋好友一起，在旅行中留下属于彼此珍贵的回忆吧~
+                </p>
+                </div>
+            <div class="right">
+                <el-form class="form" :model="form" label-position="top" label-width="120px">
+                    <el-form-item label="目的地">
+                        <el-input v-model="form.destination" />
+                    </el-form-item>
+                    <el-form-item label="出发时间">
+                        <el-date-picker
+                            v-model="form.checkIn"
+                            type="date"
+                            placeholder="yy/mm/dd"
+                            style="width: 100%"
+                        />
+                    </el-form-item>
+                    <el-form-item label="结束时间">
+                        <el-date-picker
+                            v-model="form.checkOut"
+                            type="date"
+                            placeholder="yy/mm/dd"
+                            style="width: 100%"
+                        />
+                    </el-form-item>
+                    <button>
+                        提交
+                    </button>
+                </el-form>
+            </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const form = reactive({
+  destination: '',
+  checkIn:'',
+  checkOut:''
+})
+</script>
+
+<style lang="scss" scoped>
+.JoinUs-box{
+    padding: 2.5rem 0;
+    display: flex;
+    flex-direction: column;
+    h1{
+        padding: 1.5rem;
+    }
+    .content{
+        display: flex;
+        .left{
+            width: 60%;
+            p{
+                font-size: 1.2rem;
+                padding: 0.3rem;
+                margin: .2rem  5px;
+            }
+        }
+        .right{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 40%;
+            .form{
+                width: 90%;
+                padding: 0 1rem;
+                display: flex;
+                flex-direction: column;
+                ::v-deep .el-input__wrapper {
+                    width: 100% !important;
+                }
+                ::v-deep .el-form-item__label{
+                    font-size: 18px !important;
+                }
+            }
+        }
+    }
+    
+
+}
+
+@media screen and (max-width: 760px){   
+    .content{
+        flex-direction: column;
+    .left{
+            width: 100% !important;
+    }
+    .right{
+            width: 100% !important;
+            padding: 1rem 0rem !important;
+
+    }
+
+    }
+}
+</style>
---
+++ src/components/TouristAttraction.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="container">
+        <div class="pictrue-show-box">
+            <h1>旅游胜地</h1>
+            <h4>拍摄于加勒比海</h4>
+            <div class="pictrue-box">
+                <div class="pictrue-left-box">
+                    <img src="../assets/maldives3.jpg" alt="/">
+                </div>
+                <div class="pictrue-right-box">
+                    <img src="../assets/borabora.jpg" alt="/">
+                    <img src="../assets/borabora2.jpg" alt="/">
+                    <img src="../assets/keywest.jpg" alt="/">
+                    <img src="../assets/maldives.jpg" alt="/" />
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    
+</script>
+
+<style lang="scss" scoped>
+.pictrue-show-box{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2.5rem 0;
+    h4{
+        font-weight: normal;
+        padding: 1.5rem;
+    }
+
+    .pictrue-box{
+        width: 100%;
+        height: 500px;
+        display: flex;
+        padding: 10px;
+        height: 100%;
+        .pictrue-left-box{
+            width: 60%;
+            margin: 5px;
+        }
+        .pictrue-left-box img{
+            width: 100%;
+            transition: all .3s;
+        }
+        .pictrue-left-box img:hover{
+                cursor: pointer;
+                transform: scale(1.02);
+         }
+        .pictrue-right-box{
+            display: flex;
+            flex-wrap: wrap;
+            width: 40%;
+            height: 100%;
+            img{
+                flex-grow:1;
+                width: 200px;
+                height: 186px;
+                object-fit: cover;
+                margin: 5px;
+                transition: all .3s;
+            }
+            img:hover{
+                cursor: pointer;
+                transform: scale(1.1);
+            }
+            
+            
+        }
+    }
+}
+@media screen and (max-width: 1280px) and (min-width:760px){
+    .pictrue-right-box{
+        height: auto !important;
+    }
+    .pictrue-right-box img{
+        width: inherit !important;
+        height: 45.5% !important;
+    }
+}
+@media screen and (max-width: 759px) {
+    .pictrue-box{
+        flex-direction: column;
+    }
+    .pictrue-left-box{
+        width: 90% !important;
+        margin: 0 auto !important;
+    }
+    .pictrue-right-box{
+        display: flex;
+        flex-wrap: wrap;
+        width: 91% !important;
+        margin: 5px auto !important;
+        height: 100%;
+    }
+    .pictrue-right-box{
+        height: auto !important;
+    }
+    h1{
+       font-size:2.5rem ; 
+    }
+}
+</style>
